333e4c86f37b3c667a2ed49993c02876b3fc02ef
rnikrozoft <rnikrozoft.dev@gmail.com>
2024-05-01 00:24:16 +0700

    add navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,101 +1,111 @@
 
 'use client';
 
-import { Button, MegaMenu, Navbar } from 'flowbite-react';
+import { useState } from 'react';
+import { MegaMenu, Navbar } from 'flowbite-react';
+import { HiChevronDown } from 'react-icons/hi';
 
-function Nav() {
+function Component() {
+    const [showMega, setShowMega] = useState(false);
+
     return (
-        <MegaMenu>
-            <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4 md:space-x-8">
-                <Navbar.Brand href="/">
-                    <img alt="" src="/favicon.svg" className="mr-3 h-6 sm:h-9" />
-                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
-                </Navbar.Brand>
-                <div className="order-2 hidden items-center md:flex">
-                    <a
-                        href="#"
-                        className="mr-1 rounded-lg px-4 py-2 text-sm font-medium text-gray-800 hover:bg-gray-50 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-800 md:mr-2 md:px-5 md:py-2.5"
-                    >
-                        Login
-                    </a>
-                    <Button href="#">Sign up</Button>
-                </div>
-                <Navbar.Toggle />
-                <Navbar.Collapse>
-                    <Navbar.Link href="#">Home</Navbar.Link>
-                    <Navbar.Link>
-                        <MegaMenu.Dropdown toggle={<>Company</>}>
-                            <ul className="grid grid-cols-3">
-                                <div className="space-y-4 p-4">
-                                    <li>
-                                        <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
-                                            About Us
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
-                                            Library
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
-                                            Resources
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
-                                            Pro Version
-                                        </a>
-                                    </li>
-                                </div>
-                                <div className="space-y-4 p-4">
-                                    <li>
-                                        <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
-                                            Contact Us
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
-                                            Support Center
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
-                                            Terms
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
-                                            Blog
-                                        </a>
-                                    </li>
-                                </div>
-                                <div className="space-y-4 p-4">
-                                    <li>
-                                        <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
-                                            Newsletter
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
-                                            Playground
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
-                                            License
-                                        </a>
-                                    </li>
-                                </div>
-                            </ul>
-                        </MegaMenu.Dropdown>
-                    </Navbar.Link>
-                    <Navbar.Link href="#">Team</Navbar.Link>
-                    <Navbar.Link href="#">Contact</Navbar.Link>
-                </Navbar.Collapse>
-            </div>
+        <MegaMenu className='container mx-auto'>
+            <Navbar.Brand href="/">
+                <img alt="" src="/favicon.svg" className="mr-3 h-6 sm:h-9" />
+                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse>
+                <Navbar.Link href="/">Home</Navbar.Link>
+                <Navbar.Link>
+                    <MegaMenu.DropdownToggle onClick={() => setShowMega(!showMega)}>
+                        Company
+                        <HiChevronDown className="ml-2" />
+                    </MegaMenu.DropdownToggle>
+                </Navbar.Link>
+                <Navbar.Link href="/">Marketplace</Navbar.Link>
+                <Navbar.Link href="/">Resources</Navbar.Link>
+                <Navbar.Link href="/">Contact</Navbar.Link>
+            </Navbar.Collapse>
+            {showMega && (
+                <MegaMenu.Dropdown>
+                    <ul className="mx-auto mt-6 grid border-y border-gray-200 px-4 py-5 sm:grid-cols-2 md:grid-cols-4 md:px-6">
+                        <li>
+                            <a href="#" className="block rounded-lg p-3 hover:bg-gray-50 dark:hover:bg-gray-700">
+                                <div className="font-semibold">Online Stores</div>
+                                <span className="text-sm text-gray-500 dark:text-gray-400">
+                                    Connect with third-party tools that you're already using.
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="block rounded-lg p-3 hover:bg-gray-50 dark:hover:bg-gray-700">
+                                <div className="font-semibold">Segmentation</div>
+                                <span className="text-sm text-gray-500 dark:text-gray-400">
+                                    Connect with third-party tools that you're already using.
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="block rounded-lg p-3 hover:bg-gray-50 dark:hover:bg-gray-700">
+                                <div className="font-semibold">Marketing CRM</div>
+                                <span className="text-sm text-gray-500 dark:text-gray-400">
+                                    Connect with third-party tools that you're already using.
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="block rounded-lg p-3 hover:bg-gray-50 dark:hover:bg-gray-700">
+                                <div className="font-semibold">Online Stores</div>
+                                <span className="text-sm text-gray-500 dark:text-gray-400">
+                                    Connect with third-party tools that you're already using.
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="block rounded-lg p-3 hover:bg-gray-50 dark:hover:bg-gray-700">
+                                <div className="font-semibold">Segmentation</div>
+                                <span className="text-sm text-gray-500 dark:text-gray-400">
+                                    Connect with third-party tools that you're already using.
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="block rounded-lg p-3 hover:bg-gray-50 dark:hover:bg-gray-700">
+                                <div className="font-semibold">Marketing CRM</div>
+                                <span className="text-sm text-gray-500 dark:text-gray-400">
+                                    Connect with third-party tools that you're already using.
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="block rounded-lg p-3 hover:bg-gray-50 dark:hover:bg-gray-700">
+                                <div className="font-semibold">Audience Management</div>
+                                <span className="text-sm text-gray-500 dark:text-gray-400">
+                                    Connect with third-party tools that you're already using.
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="block rounded-lg p-3 hover:bg-gray-50 dark:hover:bg-gray-700">
+                                <div className="font-semibold">Creative Tools</div>
+                                <span className="text-sm text-gray-500 dark:text-gray-400">
+                                    Connect with third-party tools that you're already using.
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="block rounded-lg p-3 hover:bg-gray-50 dark:hover:bg-gray-700">
+                                <div className="font-semibold">Marketing Automation</div>
+                                <span className="text-sm text-gray-500 dark:text-gray-400">
+                                    Connect with third-party tools that you're already using.
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                </MegaMenu.Dropdown>
+            )}
         </MegaMenu>
     );
 }
-
-export default Nav;
+export default Component;
